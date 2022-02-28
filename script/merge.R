2968fd2d3b57ab92a7d9a0df6505c0f86023ef98
Longhin <p1026u21@capri.hpcdom.local>
2022-02-28 16:46:13 +0100

    Correct merging script

--- script/merge.R
+++ script/merge.R
@@ -9,3 +9,4 @@ seqtab <- makeSequenceTable(mergers)
 dim(seqtab)
 # Inspect distribution of sequence lengths
 table(nchar(getSequences(seqtab)))
+save.image("/stor/progetti/p1026/p1026u21/HPC_project/data/workspaces/post_merging.RData")
