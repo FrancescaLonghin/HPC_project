a4b7bedd9ccaeadfaed987f7caafa43597109c33
Longhin <p1026u21@capri.hpcdom.local>
2022-02-28 09:51:17 +0100

    Correct another detail in the paths for inspection

--- script/inspect.R
+++ script/inspect.R
@@ -8,7 +8,7 @@ sample.names <- sapply(strsplit(basename(fnFs), "_R1_001.fastq.gz"),`[`, 1)
 plotQualityProfile(fnFs[1:2])
 plotQualityProfile(fnRs[1:2])
 
-path <- "~/data"
+path <- "/stor/progetti/p1026/p1026u21/HPC_project/data"
 filtFs <- file.path(path, "filtered", paste0(sample.names, "_F_filt.fastq.gz"))
 filtRs <- file.path(path, "filtered", paste0(sample.names, "_R_filt.fastq.gz"))
 names(filtFs) <- sample.names
